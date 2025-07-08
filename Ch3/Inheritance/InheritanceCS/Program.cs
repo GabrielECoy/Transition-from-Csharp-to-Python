af07f6b8c0157b0bda07ef734898290598506c71
Gabriel Coy <GabrielCoy@outlook.com>
2025-07-08 16:34:27 -0400

    .net9 conversion of inheritance project

--- Ch3/Inheritance/InheritanceCS/Program.cs
+++ Ch3/Inheritance/InheritanceCS/Program.cs
@@ -1,61 +1,23 @@
 ﻿using System;
 
-namespace InheritanceCS
+// .NET 9 top-level statements and primary constructors
+Book b1 = new Book("Leo Tolstoy", "War and Peace", 21.95m);
+Book b2 = new Book("Aldous Huxley", "Brave New World", 24.95m);
+
+Console.WriteLine(b1);
+Console.WriteLine(b1.Title);
+Console.WriteLine(b2);
+Console.WriteLine(b2.Author);
+
+class Publication(string Title, decimal Price)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Book b1 = new Book("Leo Tolstoy", "War and Peace", 21.95m);
-            Book b2 = new Book("Aldous Huxley", "Brave New World", 24.95m);
+    public string Title { get; set; } = Title;
+    public decimal Price { get; set; } = Price;
+}
 
-            Console.WriteLine(b1);
-            Console.WriteLine(b1.mTitle);
-            Console.WriteLine(b2);
-            Console.WriteLine(b2.mAuthor);
-        }
-    }
+class Book(string Title, string Author, decimal Price) : Publication(Title, Price)
+{
+    public string Author { get; set; } = Author;
 
-    class Publication
-    {
-        protected string _mTitle;
-        protected decimal _mPrice;
-
-        public Publication(string title, decimal price)
-        {
-            _mTitle = title;
-            _mPrice = price;
-        }
-    }
-
-    class Book : Publication
-    {
-        protected string _mAuthor;
-
-        public Book(string title, string author, decimal price) :
-            base(title, price)
-        {
-            _mAuthor = author;
-        }
-        public override string ToString()
-        {
-            return $"{mTitle} by {mAuthor}, price ${mPrice}";
-        }
-
-        public string mTitle
-        {
-            get { return _mTitle; }
-            set { _mTitle = value; }
-        }
-        public string mAuthor
-        {
-            get { return _mAuthor; }
-            set { _mAuthor = value; }
-        }
-        public decimal mPrice
-        {
-            get { return _mPrice; }
-            set { _mPrice = value; }
-        }
-    }
+    public override string ToString() => $"{Title} by {Author}, price ${Price}";
 }
