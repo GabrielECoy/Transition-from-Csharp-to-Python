0a473fa24a0f452ab32cf192df08945ca3f17e43
Gabriel Coy <GabrielCoy@outlook.com>
2025-07-08 12:50:17 -0400

    Adding changes

--- Ch2/FunctionsCS/Program.cs
+++ Ch2/FunctionsCS/Program.cs
@@ -1,82 +1,74 @@
-﻿using System;
+﻿// .NET 9: Top-level statements, no namespace/class/entry point needed
+using System;
 using System.Collections.Generic;
 
-namespace FunctionsCS
+// call a function with a return value
+int result = RegularFunction(5, 10);
+Console.WriteLine(result);
+
+// call a function with named parameters
+NamedParams(5, "world", false);
+NamedParams(c: true, b: "hello", a: 5);
+
+// use the variable arguments example
+VariableParams(1, 'a', 3.05f, "test");
+
+// Try the lambda function
+LambdaFunc();
+
+int RegularFunction(int a, int b)
 {
-    class Program
+    // C# lets you change the value of an "out" parameter
+    Console.WriteLine($"{a + b}");
+
+    // This is a function that returns an integer
+    return 42;
+}
+
+void NamedParams(int a, string b, bool c = false)
+{
+    // This function returns nothing
+    if (c)
     {
-        static void Main(string[] args)
-        {
-            // call a function with a return value
-            int result = RegularFunction(5, 10);
-            Console.WriteLine(result);
+        Console.WriteLine("'a' comes first");
+        Console.WriteLine("{0}", a);
+        Console.WriteLine("{0}", b);
+    }
+    else
+    {
+        Console.WriteLine("'b' comes first");
+        Console.WriteLine("{0}", b);
+        Console.WriteLine("{0}", a);
+    }
+}
 
-            // call a function with named parameters
-            NamedParams(5, "world", false);
-            NamedParams(c: true, b: "hello", a: 5);
+void VariableParams(int a, int b, params object[] args)
+{
+    Console.WriteLine($"{a + b}");
+    foreach (object arg in args)
+    {
+        Console.WriteLine("Argument: " + arg);
+    }
+}
 
-            // use the variable arguments example
-            VariableParams(1, 'a', 3.05f, "test");
+void LambdaFunc()
+{
+    // define a list of integers
+    List<int> data = new List<int> { 10, 6, 23, 15, 18, 59, 62, 7, 103, 29, 35 };
 
-            // Try the lambda function
-            LambdaFunc();
-        }
+    // Sort the list using default sorting
+    Console.WriteLine("Regular sort:");
+    data.Sort();
+    foreach (var i in data)
+    {
+        Console.WriteLine(i);
+    }
 
-        static int RegularFunction(int a, int b)
-        {
-            // C# lets you change the value of an "out" parameter
-            Console.WriteLine($"{a + b}");
-
-            // This is a function that returns an integer
-            return 42;
-        }
-
-        static void NamedParams(int a, string b, bool c = false)
-        {
-            // This function returns nothing
-            if (c)
-            {
-                Console.WriteLine("'a' comes first");
-                Console.WriteLine("{0}", a);
-                Console.WriteLine("{0}", b);
-            }
-            else
-            {
-                Console.WriteLine("'b' comes first");
-                Console.WriteLine("{0}", b);
-                Console.WriteLine("{0}", a);
-            }
-        }
-
-        static void VariableParams(int a, int b, params object[] args)
-        {
-            Console.WriteLine($"{a + b}");
-            foreach (object arg in args)
-            {
-                Console.WriteLine("Argument: " + arg);
-            }
-        }
-
-        static void LambdaFunc()
-        {
-            // define a list of integers
-            List<int> data = new List<int> { 10, 6, 23, 15, 18, 59, 62, 7, 103, 29, 35 };
-
-            // Sort the list using default sorting
-            Console.WriteLine("Regular sort:");
-            data.Sort();
-            foreach (var i in data)
-            {
-                Console.WriteLine(i);
-            }
-
-            // Sort the list with a lambda function
-            Console.WriteLine("Sort by first digit:");
-            data.Sort((a, b) => (a.ToString()[0].CompareTo(b.ToString()[0])));
-            foreach (var i in data)
-            {
-                Console.WriteLine(i);
-            }
-        }
+    // Sort the list with a lambda function
+    Console.WriteLine("Sort by first digit:");
+    data.Sort((a, b) => (a.ToString()[0].CompareTo(b.ToString()[0])));
+    foreach (var i in data)
+    {
+        Console.WriteLine(i);
     }
 }
